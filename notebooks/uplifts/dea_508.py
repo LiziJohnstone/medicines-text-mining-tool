302d588d2cb975e59c8a54431684eefeee5cd1e5
Lizi Johnstone <105051948+LiziJohnstone@users.noreply.github.com>
2026-03-12 09:52:27 +0000

    Delete notebooks/uplifts directory

--- notebooks/uplifts/dea_508.py
+++
@@ -1,8 +0,0 @@
-# Databricks notebook source
-dbutils.widgets.text('db', '', 'db')
-DB = dbutils.widgets.get('db')
-assert DB
-
-# COMMAND ----------
-
-spark.sql(f"DROP TABLE IF EXISTS {DB}.responses")
